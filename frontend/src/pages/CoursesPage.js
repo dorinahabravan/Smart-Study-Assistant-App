ed65eaeb6b2db033fa2a046087c197f92da20c3f
dorinahabravan <dorina.habravan@gmail.com>
2025-04-16 01:34:02 +0100

    Search functionallity

--- frontend/src/pages/CoursesPage.js
+++ frontend/src/pages/CoursesPage.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Container, Typography, Grid, Card, CardContent, Button,  CircularProgress, Box} from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { TextField } from "@mui/material";
+
 
 
 
 const CoursesPage = () => {
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -33,14 +37,29 @@ const CoursesPage = () => {
       <Typography variant="h4" gutterBottom>
         📚 Available Courses
       </Typography>
-
+      <TextField
+  label="Search courses or topics..."
+  variant="outlined"
+  fullWidth
+  value={searchTerm}
+  onChange={(e) => setSearchTerm(e.target.value)}
+  sx={{ marginBottom: "2rem" }}
+/>
       {loading ? (
+      
         <Box display="flex" justifyContent="center" mt={5}>
           <CircularProgress />
         </Box>
       ) : (
         <Grid container spacing={3}>
-          {courses.map((course, index) => (
+          {courses
+  .filter(course =>
+    course.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    course.subtopics.some(sub =>
+      sub.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  )
+  .map((course, index) => (
             <Grid item xs={12} sm={6} md={4} key={index}>
               <Card>
                 <CardContent>
